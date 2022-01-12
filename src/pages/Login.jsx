79b740ab3d405e6b1eca880b82626a03b83f82f6
Guarize <pedroguarize@hotmail.com>
2022-01-11 22:06:09 -0300

    [fix] - lint

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AiOutlineUser } from 'react-icons/ai';
 import { RiLockPasswordLine } from 'react-icons/ri';
